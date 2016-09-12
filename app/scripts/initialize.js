7db394678606aeb64b66694b5279d12e02e0d077
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-09-12 21:40:29 +0300

    Added Redux file structure

--- app/scripts/initialize.js
+++ app/scripts/initialize.js
@@ -1,15 +1,24 @@
 import '../styles/main.scss';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+
+
+const store = createStore(
+  combineReducers({})
+);
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.createElement('div');
   el.id = 'app';
   document.body.appendChild(el);
-  ReactDOM.render(
-    <h1>Hello, world!</h1>,
+  render(
+    <Provider store={store}>
+      <h1>Hello, world!</h1>
+    </Provider>,
     el
   );
 });
