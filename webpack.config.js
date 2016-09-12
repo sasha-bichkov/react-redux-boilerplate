9f20c04a0274a78363763fcf3f23cab2e414605f
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-09-12 17:57:29 +0300

    Configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    app: path.join(__dirname, 'app/scripts', 'initialize.js')
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    //stats: 'errors-only',
+  },
+  watchOptions: {
+    // Delay the rebuild after the first change
+    aggregateTimeout: 300,
+    // Poll using interval (in ms, accepts boolean too)
+    poll: 1000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({title: 'Artists'}),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
